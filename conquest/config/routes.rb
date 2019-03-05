84a6373a23dbcbee7514f1ed327d1de82a63d482
shannon-nabors <shannon.nabors@gmail.com>
2019-03-05 15:01:11 -0500

    added custom route to update all territories at once

--- conquest/config/routes.rb
+++ conquest/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # get 'territory/index'
-  # get 'territory/show'
-  # get 'territory/edit'
-  # get 'territory/update'
   resources :territories, except: [:new, :create, :delete]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  patch "update/territories", to: "territories#update_all"
 end
